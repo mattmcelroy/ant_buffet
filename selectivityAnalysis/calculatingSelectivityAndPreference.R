2ee8182b9c75911b5dc90792ee7a985edef0d627
mattmcelroy <mattmcelroy@berkeley.edu>
2019-07-12 15:05:03 -0400

    updated some annotations

--- selectivityAnalysis/calculatingSelectivityAndPreference.R
+++ selectivityAnalysis/calculatingSelectivityAndPreference.R
@@ -1,4 +1,3 @@
-
 # Toads versus Ants
 # R analysis associated with Copeia publication "Ant morphology mediates diet preference in a neotropical toad". 
 # Matt McElroy & David Donoso
@@ -28,56 +27,54 @@ head(df)
 # (1) Calculate linear selectivity metric for each ant species.
 ##############################################################################################################
 
-# Selectivity Metrics (Linear selectivity = r - p)
+# Selectivity Metric 
+# (Linear selectivity = r - p)
+
 
 preyprop = df$stomach / (sum(df$stomach)) # calculate proportion for prey species in stomach 
 envprop = df$abundance / (sum(df$abundance)) # calculate proportion for prey species in environment 
 
-Linear = (preyprop - envprop)
+Linear = (preyprop - envprop) # calculate selectivity for each ant species
 
-df$Linear <- Linear # adding selectivity metric to dataframe
+df$Linear <- Linear # adding selectivity to dataframe
 head(df)
 
 ##############################################################################################################
 # (2) Use environmental abundances to simulate null expectations of selectivity metric for ant species.
 ##############################################################################################################
 
-species <- df$Ant_species
-specieslist <- as.list(species)                # create list of ant species in the dataset
-df2 <- cbind.data.frame(species, Linear)
+species <- df$Ant_species # create species object (factor) for ease
+df2 <- cbind.data.frame(species, Linear) # make a new df for simulated values
+df2[species=="Ectatomma_ruidum",] # double check that values match species
 
-head(df2)
-df2[species=="Ectatomma_ruidum",]
-
-for (i in 1:1000){                                          # loop over the # of simulations
-  j <- sample(2000:5000,1)                                  # sets the number of individual samples to obtain for the simulation
-  randomdraw <- sample(species, 
-                       size = j, 
+for (i in 1:1000){                                          # number of loops
+  j <- sample(2000:5000,1)                                  # number of ants sampled
+  randomdraw <- sample(species,                             # draw species from env. proportion with replacement
+                       size = j,
                        replace = TRUE, 
-                       prob = envprop)                      # draw species from env. proportion with replacement
-  df_randomdraw <- as.data.frame(table(randomdraw))         # make a dataframe of random draw species counts
-  preyprop_sim <- df_randomdraw$Freq/j                      # simulated prey species proportions
+                       prob = envprop)                      
+  df_randomdraw <- as.data.frame(table(randomdraw))         # dataframe of species counts
+  preyprop_sim <- df_randomdraw$Freq/j                      # vector of simulated prey species proportions
   Linear_sim <- as.data.frame(preyprop_sim - envprop)       # simulated LinearSelectivity by subtracting REAL env_prop from SIMULATEd prey_prop + make dataframe
   colnames(Linear_sim) <- paste0("sim",i)                   # rename column from LinearS_sim --> sim1, sim2, sim3,...sim1000
-  
-  df2 <- cbind(df2,Linear_sim) # add the simulated dataset to the dataframe with species, LinearS, sim1, sim2,...etc....
-  
-}
 
+  # add the simulated dataset to the dataframe with species, LinearS, sim1, sim2,...etc....  
+  df2 <- cbind(df2,Linear_sim) 
+
+}
 
 head(df2)
 
-L_simulations <- df2 
+# Melt the dataframes for plotting
+melted.df.L_real <- melt(df2, id.vars = "species", measure.vars = c("Linear") )
+melted.df.L_sim <- melt(df2, id.vars = "species", measure.vars = paste0("sim",c(1:1000)) )
 
-melted.df.L_real <- melt(L_simulations, id.vars = "species", measure.vars = c("Linear") )
-melted.df.L_sim <- melt(L_simulations, id.vars = "species", measure.vars = paste0("sim",c(1:1000)) )
-
-# Re-order from alphabetical to the ordered by "real" Linear Selectivity - apply to both datasets
+# Re-order the species names (factors) by "real" Linear Selectivity - apply to both datasets
 melted.df.L_real$species <- factor(melted.df.L_real$species, levels = melted.df.L_real$species[order(melted.df.L_real$value)])
-melted.df.L_sim$species <- factor(melted.df.L_sim$species, levels = melted.df.L_sim$species[order(melted.df.L_real$value)]) #order the simulated data by the real data
+melted.df.L_sim$species <- factor(melted.df.L_sim$species, levels = melted.df.L_sim$species[order(melted.df.L_real$value)])
 
 ggplot() +
-  ggtitle("Null Linear S vs. stomach contents") +
+  ggtitle("Null Distribution vs. Sampled Selectivity") +
   xlab("LinearS") +
   ylab("Ant species") +
   geom_point(data = melted.df.L_sim, aes(x = value, y = species), col = "gray") +
